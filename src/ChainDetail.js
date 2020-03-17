a8f0b4a0b46d300178e243cb4b73f5e1c298f8a6
Sam Van Kooten <vankooten.sam@gmail.com>
2020-03-17 16:19:13 -0600

    If src and dest authors are the same, show that author's publications

--- src/ChainDetail.js
+++ src/ChainDetail.js
@@ -5,12 +5,15 @@ import './ChainDetail.css';
 class ChainDetail extends React.Component {
     render() {
         const items = [];
-        for (let i = 0; i < this.props.chain.length - 1; i++) {
+        let chain = this.props.chain;
+        if (chain.length === 1)
+            chain = [chain[0], chain[0]];
+        for (let i = 0; i < chain.length - 1; i++) {
             items.push(
-                <ChainDetailItem author={this.props.chain[i]}
-                                 nextAuthor={this.props.chain[i + 1]}
+                <ChainDetailItem author={chain[i]}
+                                 nextAuthor={chain[i + 1]}
                                  repo={this.props.repo}
-                                 key={this.props.chain[i] + this.props.chain[i + 1]}
+                                 key={chain[i] + chain[i + 1]}
                                  addExclusion={this.props.addExclusion}
                                  sortOption={this.props.sortOption}
                 />)
