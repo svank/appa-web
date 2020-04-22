35701c2c82343338ca0a8a2fe421edc4ee4cbee0
Sam Van Kooten <vankooten.sam@gmail.com>
2020-04-21 18:43:19 -0600

    Update name syntax help after server-side changes

--- src/NameSyntaxHelp.js
+++ src/NameSyntaxHelp.js
@@ -31,36 +31,42 @@ function NameSyntaxHelpContents() {
             <ul>
                 <li>
                     First and middle names or initials are optional (as are {}
-                    periods after initials).
+                    periods after initials), and case is ignored.
                 </li>
                 <li>
                     A search will {}
                     match all names <i>consistent</i> with the {}
                     name you give {}
-                    (e.g. "Last, First" will match "Last, F" {}
-                    and "Last, First M").
+                    (e.g. "Last, First" will match "Last, F", {}
+                    "Last, First", and "Last, First M").
                 </li>
                 <li>
-                    Like ADS, type '<b>=</b>' before a name {}
+                    Like with ADS, type '<b>=</b>' before a name {}
                     to match only exactly what you type (so {}
                     "=Last, First" will <i>not</i> match {}
                     "Last, F").
                 </li>
             </ul>
-            APPA adds two additional modifiers like the '=' {}
-            modifier.
+            APPA adds <i>specificity</i> modifiers which function like the {}
+            '=' modifier.
             <ul>
                 <li>
-                    '<b>&lt;</b>' will match what you type or {}
-                    anything <i>less specific</i>.
+                    '<b>&lt;</b>' will match anything <i>less specific</i> {}
+                    than the name provided.
                 </li>
                 <li>
-                    '<b>&gt;</b>' will match what you type or {}
-                    anything <i>more specific</i>.
+                    '<b>&gt;</b>' will match anything <i>more specific</i> {}
+                    than the name provided.
                 </li>
                 <li>
-                    E.g. "&gt;Last, First" will match "Last, First" and {}
-                    "Last, First M", but will <i>not</i> match "Last, F".
+                    '<b>&gt;=</b>' and '<b>&lt;=</b>' will match exactly {}
+                    what you type and anything more/less specific.
+                </li>
+                <li>
+                    E.g. given the possibilities "Last, F.", "Last, First" {}
+                    and "Last, First M.", the latter two will be matched by {}
+                    "&gt;=Last, First", while only the last choice will be {}
+                    matched by "&gt;Last, First".
                 </li>
             </ul>
             "Specific" is defined by a score, the sum of: 100 {}
