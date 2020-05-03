c4f5706c7d1c883ca769d365efe77458bf0fed47
Sam Van Kooten <vankooten.sam@gmail.com>
2020-05-03 12:46:43 -0600

    Add Constellation Sketcher to loading screen

--- src/LoadingDisplay.js
+++ src/LoadingDisplay.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './LoadingDisplay.css'
+import ConstellationSketcher, {categories} from 'react-constellation-sketcher'
 
 class LoadingDisplay extends React.Component {
+    constructor(props) {
+        super(props);
+        this.startingConst = categories.popular[
+            Math.floor(Math.random() * categories.popular.length)];
+    }
     render() {
         let content;
         if (this.props.data === null
             || this.props.data.isDummy)
-            content = <div />;
+            content = <div className={"LoadStatus"}>
+                <div className="LoadStatusSubtitle">&nbsp;</div>
+                <div className="LoadStatusPiece">&nbsp;</div>
+            </div>;
         else {
             const nAuths = this.props.data.n_authors_queried;
             const authsNoun = nAuths === 1 ? "author" : "authors";
@@ -15,7 +24,7 @@ class LoadingDisplay extends React.Component {
             const nADS = this.props.data.n_ads_queries;
             const adsNoun = nADS === 1 ? "query" : "queries";
             content = (
-                <div className="LoadStatus">
+                <div className="LoadStatus text-muted">
                     <div className="LoadStatusSubtitle">
                         Progress:
                     </div>
@@ -31,9 +40,37 @@ class LoadingDisplay extends React.Component {
                 </div>
             );
         }
+        
         return (
             <div className="LoadingDisplay">
-                <div className="LoadingTitle">Loading...</div>
+                <div className="LoadingTitle">Finding Connections...</div>
+                <ConstellationSketcher width={500}
+                                       height={500}
+                                       slideshow={true}
+                                       speedScale={1.2}
+                                       slideshowDwellTime={2250}
+                                       sizeScale={1.1}
+                                       constellation={this.startingConst}
+                                       weights={{
+                                           popular: 20,
+                                           striking: 40,
+                                           medium: 8,
+                                           small: 1,
+                                       }}
+                                       style={{
+                                           margin: "15px",
+                                           borderRadius: "10px",
+                                       }}
+                                       drawFrameCompleteCallback={
+                                           (ctx, redrew, constellation) => {
+                                               if (!redrew)
+                                                   return;
+                                               ctx.font = "11pt Arial";
+                                               ctx.fillStyle = "rgb(170,152,130)";
+                                               ctx.fillText(constellation, 6, 492);
+                                           }
+                                       }
+                />
                 {content}
             </div>
         )
