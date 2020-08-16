3f8b744a1a6c35f45f1500598cbe188af498261f
Sam Van Kooten <vankooten.sam@gmail.com>
2020-08-16 14:40:46 -0600

    Handle plurals correctly in StatsDisplay

--- src/StatsDisplay.js
+++ src/StatsDisplay.js
@@ -12,6 +12,8 @@ const StatsDisplay = React.memo(props => {
         }
     }
     
+    const nDocuments = Object.keys(props.repo.docData).length;
+    
     return (
         <div className="stats-display-container">
             <Button variant="link"
@@ -31,24 +33,40 @@ const StatsDisplay = React.memo(props => {
             >
                 <Popover id="popover-positioned-bottom">
                     <Popover.Content>
-                        Queried {props.stats.n_authors_queried} authors and {}
-                        checked {props.stats.n_docs_queried} documents. {}
-                        A total of {props.stats.n_authors_from_ads} authors {}
-                        were queried from ADS in {}
-                        {props.stats.n_network_queries} queries, which took {}
+                        Queried {props.stats.n_authors_queried} author
+                        {props.stats.n_authors_queried === 1 ? '' : 's'} and {}
+                        checked {props.stats.n_docs_queried} document
+                        {props.stats.n_docs_queried === 1 ? '' : 's'}. {}
+                        A total of {props.stats.n_authors_from_ads} author
+                        {props.stats.n_authors_from_ads === 1 ? '' : 's'} {}
+                        {props.stats.n_authors_from_ads === 1 ? 'was' : 'were'} {}
+                        queried from ADS in {}
+                        {props.stats.n_network_queries} quer
+                        {props.stats.n_network_queries === 1 ? 'y' : 'ies'}, {}
+                        which took {}
                         {props.stats.time_waiting_network.toFixed(2)} {}
                         of the {}
                         {props.stats.total_time.toFixed(2)} {}
                         seconds spent searching. (When possible, ADS query {}
                         results are re-used from past searches.) A total of {}
-                        {props.stats.n_names_seen} (seemingly) unique names {}
-                        were seen on author lists.
+                        {props.stats.n_names_seen} (seemingly) unique name
+                        {props.stats.n_names_seen === 1 ? '' : 's'} {}
+                        {props.stats.n_names_seen === 1 ? 'was' : 'were'} {}
+                        seen on author lists.
+                        
                         <br /><br />
-                        A total of {Object.keys(props.repo.docData).length} {}
-                        papers link {authorSet.size} authors in {}
-                        {props.repo.chains.length} chains of coauthorship {}
-                        connecting {props.repo.originalSource} {}
-                        and {props.repo.originalDest}.
+                        
+                        A total of {nDocuments} paper
+                        {nDocuments === 1 ? '' : 's'} {}
+                        link{nDocuments === 1 ? 's' : ''} {}
+                        {authorSet.size} author
+                        {authorSet.size === 1 ? '' : 's'} in {}
+                        {props.repo.chains.length} chain
+                        {props.repo.chains.length === 1 ? '' : 's'} {}
+                        of coauthorship connecting {}
+                        {props.repo.originalSource} {}
+                        and {props.repo.originalDest}
+                        {props.repo.originalDest.endsWith('.') ? '' : '.'}
                     </Popover.Content>
                 </Popover>
             </Overlay>
